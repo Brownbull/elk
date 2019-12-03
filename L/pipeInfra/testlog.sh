7c0a60b696ae45eabb8f7ef42fc50f0316541b34
unknown <carcamo.gabriel@gmail.com>
2019-12-03 10:41:26 -0300

    working on mongo2logstash

--- L/pipeInfra/testlog.sh
+++ L/pipeInfra/testlog.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "11/18/2019 0:00" | logstash -e 'input { stdin {} } filter { date { match => [ "message", "ISO8601", "yyyy-MM-dd HH:mm:ss ZZZ"] } }'
+echo "11/18/2019 0:00" | logstash -e 'input { stdin {} } filter { date { match => [ "message", "ISO8601", "MM/dd/yyyy HH:mm:ss ZZZ"] } }'

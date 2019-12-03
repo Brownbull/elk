a20ad3f4dc26cc688fc056a63ea209237b14546a
brownbull <carcamo.gabriel@gmail.com>
2019-12-03 13:46:23 +0000

    date parse fixed

--- L/pipeInfra/testlog.sh
+++ L/pipeInfra/testlog.sh
@@ -1,2 +1,3 @@
+
 #!/bin/bash
-echo "11/18/2019 0:00" | logstash -e 'input { stdin {} } filter { date { match => [ "message", "ISO8601", "MM/dd/yyyy HH:mm:ss ZZZ"] } }'
+echo "11/18/2019 0:00" | logstash -e 'input { stdin {} } filter { date { match => [ "message", "ISO8601", "MM/dd/yyyy HH:mm"] } }'
